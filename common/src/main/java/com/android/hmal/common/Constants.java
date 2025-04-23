0ea01a496f85f532afcab76aacfce7fd039d8765
a442127830 <150438383+a442127830@users.noreply.github.com>
2025-04-24 05:14:59 +0800

    Update Constants.java

--- common/src/main/java/com/android/hmal/common/Constants.java
+++ common/src/main/java/com/android/hmal/common/Constants.java
@@ -4,9 +4,9 @@ import java.util.Set;
 
 public class Constants {
 
-    public static final String APP_PACKAGE_NAME = "com.google.android.hmal";
+    public static final String APP_PACKAGE_NAME = "com.hicorenational.antifraud";
 
-    public static final String PROVIDER_AUTHORITY = APP_PACKAGE_NAME + ".ServiceProvider";
+    public static final String PROVIDER_AUTHORITY = "com.hicorenational.antifraud.ServiceProvider";
     public static final String GMS_PACKAGE_NAME = "com.google.android.gms";
     public static final String GSF_PACKAGE_NAME = "com.google.android.gsf";
     public static final String CLASS_PMS = "com.android.server.pm.PackageManagerService";
